6ce7fe3b42b8f410a9bc87b1a372352a1e65519b
Maximillian Dornseif <md@foxel.org>
2022-02-20 21:11:36 +0100

    fix: typings

--- src/lib/Sidebar.tsx
+++ src/lib/Sidebar.tsx
@@ -10,7 +10,7 @@ import { Stack } from '@fluentui/react/lib/Stack'
 import React from 'react'
 import { Route, useLocation, useRouter } from 'wouter'
 
-export interface ISidebarNavLink extends INavLink {
+export interface ISidebarNavLink extends Omit<INavLink, 'name'& 'url'> {
   component: JSX.Element
 }
 
@@ -26,11 +26,11 @@ export function WithSidebar(props: { items: ISidebarProps; children?: React.Reac
   const navLinkGroups = []
   const navContent = []
   let selectedKey: string = ''
-  for (const [grname, gr] of Object.entries(props.items)) {
+  for (const [groupName, groupData] of Object.entries(props.items)) {
     const links = []
-    let collapseByDefault: boolean = gr.collapseByDefault || false
-    for (const item of gr.links) {
-      const key = `${grname}-${item?.title}`
+    let collapseByDefault: boolean = groupData.collapseByDefault || false
+    for (const item of groupData.links) {
+      const key = `${groupName}-${item?.title}`
       const [match] = router.matcher(`${item.path}/:p*`, location)
       // invariant(!item?.path.endsWith('/'), `${item?.path} must not end with /`)
       if (match) {
@@ -49,8 +49,8 @@ export function WithSidebar(props: { items: ISidebarProps; children?: React.Reac
       )
     }
     navLinkGroups.push({
-      ...gr,
-      name: grname,
+      ...groupData,
+      name: groupName,
       links,
       selectedKey,
       collapseByDefault,
